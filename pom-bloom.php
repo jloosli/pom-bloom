229c4af70e469af88e46bf7cc6b5d2e3f55daff6
Jared Loosli <jloosli@gmail.com>
2014-11-05 10:24:51 -0700

    Initial Commit

---
+++ pom-bloom.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Plugin Name: POM Bloom
+ * Version: 1.0
+ * Plugin URI: http://www.hughlashbrooke.com/
+ * Description: This is your starter template for your next WordPress plugin.
+ * Author: Hugh Lashbrooke
+ * Author URI: http://www.hughlashbrooke.com/
+ * Requires at least: 4.0
+ * Tested up to: 4.0
+ *
+ * Text Domain: pom-bloom
+ * Domain Path: /lang/
+ *
+ * @package WordPress
+ * @author Hugh Lashbrooke
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Load plugin class files
+require_once( 'includes/class-pom-bloom.php' );
+require_once( 'includes/class-pom-bloom-settings.php' );
+
+// Load plugin libraries
+require_once( 'includes/lib/class-pom-bloom-admin-api.php' );
+require_once( 'includes/lib/class-pom-bloom-post-type.php' );
+require_once( 'includes/lib/class-pom-bloom-taxonomy.php' );
+
+/**
+ * Returns the main instance of POM_Bloom to prevent the need to use globals.
+ *
+ * @since  1.0.0
+ * @return object POM_Bloom
+ */
+function POM_Bloom () {
+	$instance = POM_Bloom::instance( __FILE__, '1.0.0' );
+
+	if ( is_null( $instance->settings ) ) {
+		$instance->settings = POM_Bloom_Settings::instance( $instance );
+	}
+
+	return $instance;
+}
+
+POM_Bloom();
