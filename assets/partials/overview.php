24b8387652cdaf8370f4cb50946ad136f7603a63
Jared Loosli <jloosli@gmail.com>
2014-12-19 11:11:13 -0700

    Updated Bloom so missing goalsets didn't throw off the overview page

--- assets/partials/overview.php
+++ assets/partials/overview.php
@@ -9,33 +9,37 @@
 
         <?php foreach ( $goalsets as $gs ): ?>
             <th>
-                <?php printf("<a href='%s?page=goals.update&goalset=%s'>%s</a>",
-                    explode("?",$_SERVER["REQUEST_URI"])[0] ,
+                <?php printf( "<a href='%s?page=goals.update&goalset=%s'>%s</a>",
+                    explode( "?", $_SERVER["REQUEST_URI"] )[0],
                     $gs->name,
-                    $gs->name); ?>
+                    $gs->name ); ?>
             </th>
         <?php endforeach; ?>
         <th>Total</th>
     </tr>
     </thead>
     <tbody>
-    <?php $total_gs = []; foreach ( $categories as $cat ):  ?>
+    <?php $total_gs = [ ];
+    foreach ( $categories as $cat ): ?>
         <tr>
             <th><?php echo $cat->name; ?></th>
-            <?php  $total = 0; foreach($goalsets as $gs): $total_gs[$gs->name] = 0;?>
-            <td><?php
-                    foreach($goals[$gs->name][$cat->name] as $goal) {
-                        printf("<img title = '%s' src='%s' /> ",
-                            $goal->post_title,
-                            $this->parent->assets_url.'/images/' .
-                            ($goal->is_completed ? 'accept' : 'cross') .
-                                                             '.png'
-                        );
+            <?php $total = 0;
+            foreach ( $goalsets as $gs ): $total_gs[ $gs->name ] = 0; ?>
+                <td><?php
+                    if ( !empty( $goals ) ) {
+                        foreach ( $goals[ $gs->name ][ $cat->name ] as $goal ) {
+                            printf( "<img title = '%s' src='%s' /> ",
+                                $goal->post_title,
+                                $this->parent->assets_url . '/images/' .
+                                ( $goal->is_completed ? 'accept' : 'cross' ) .
+                                '.png'
+                            );
 
-                        $total += $goal->is_completed;
-                        $total_gs[$gs->name] += $goal->is_completed;
+                            $total += $goal->is_completed;
+                            $total_gs[ $gs->name ] += $goal->is_completed;
 
-                        } ?>
+                        }
+                    } ?>
                 </td>
             <?php endforeach; ?>
             <th><?php echo $total; ?></th>
@@ -43,27 +47,30 @@
     <?php endforeach; ?>
     <tr>
         <th>Serendipity</th>
-        <?php $total=0; foreach($goalsets as $gs): ?>
+        <?php $total = 0;
+        foreach ( $goalsets as $gs ): ?>
             <td><?php
-                foreach($goals[$gs->name]['serendipity'] as $goal) {
-                    printf( "<img src='%s' /> ",
-                        $this->parent->assets_url . '/images/' .
-                        ( $goal->is_completed ? 'accept' : 'cross' ) .
-                        '.png'
-                    );
-                    $total += $goal->is_completed;
-                    $total_gs[$gs->name] += $goal->is_completed;
-                } ?>
+                if(!empty($goals)) {
+                    foreach ( $goals[ $gs->name ]['serendipity'] as $goal ) {
+                        printf( "<img src='%s' /> ",
+                            $this->parent->assets_url . '/images/' .
+                            ( $goal->is_completed ? 'accept' : 'cross' ) .
+                            '.png'
+                        );
+                        $total += $goal->is_completed;
+                        $total_gs[ $gs->name ] += $goal->is_completed;
+                    }
+                }?>
             </td>
         <?php endforeach; ?>
         <th><?php echo $total; ?></th>
     </tr>
     <tr>
         <th>Totals</th>
-        <?php foreach($goalsets as $gs): ?>
-            <th><?php echo $total_gs[$gs->name]; ?></th>
+        <?php foreach ( $goalsets as $gs ): ?>
+            <th><?php echo $total_gs[ $gs->name ]; ?></th>
         <?php endforeach; ?>
-        <th><?php echo array_sum($total_gs); ?></th>
+        <th><?php echo array_sum( $total_gs ); ?></th>
     </tr>
     </tbody>
 </table>
@@ -71,8 +78,9 @@
 <ul class="actions">
     <li><a href="javascript:window.location.search='page=instructions'">Read over the instructions for Bloom</a></li>
     <li><a href="javascript:window.location.search='page=preferences'">Update your game preferences</a></li>
-    <li><a href="javascript:window.location.search='page=assessments'">Look at previously completed self assessments</a></li>
-    <?php if($this->canUserAddGoals($current_user->ID)): ?>
-    <li><a href="javascript:window.location.search='page=goals.set'">Set some new goals</a></li>
+    <li><a href="javascript:window.location.search='page=assessments'">Look at previously completed self assessments</a>
+    </li>
+    <?php if ( $this->canUserAddGoals( $current_user->ID ) ): ?>
+        <li><a href="javascript:window.location.search='page=goals.set'">Set some new goals</a></li>
     <?php endif; ?>
 </ul>
